ed489190924584e6f1010110732f86c261526300
Scarlett <hwang.scarlett@gmail.com>
2020-01-13 11:52:00 -0800

    split function added

--- convert.sh
+++ convert.sh
@@ -4,13 +4,20 @@
 
 # For every .txt file within r_string_diff folder
 for filename in *.txt ; do
+  # if splitting a file needed, use code below
+  # split $filename prefix
+  # for subfile in prefix% ; do
+
   # Extract just the name of the file without extention
   jpg=${filename%.txt}
+
   # Convert .txt to .jpg (ONLY FIRST PAGE WORKS) and export
   /Applications/LibreOffice.app/Contents/MacOS/soffice --convert-to jpg "$filename"
+
   # Add dither
   # magick $jpg.jpg -define dither:diffusion-amount=1000% "$jpg-p.jpg"
   magick $jpg.jpg -dither Riemersma -colors 2 $jpg-d.jpg
+
   # Do OCR on the created .jpg file and export
   tesseract $jpg-d.jpg "$jpg-converted"
 done
