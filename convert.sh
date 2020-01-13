b68e4cd3d310302b2fcef3c566f9cb9f6b31dd74
Scarlett <hwang.scarlett@gmail.com>
2020-01-12 19:43:41 -0800

    pipeline working

--- convert.sh
+++ convert.sh
@@ -2,20 +2,6 @@
 # OCR Text-image File Conversion Script
 # January 10th, 2020
 
-# convert -density 200 Hládek2017_Article_LearningStringDistanceWithSmoo.pdf test1.png
-# for i in *.pdf ; do
-#   name=${i%.*}
-#   png=$name.png
-#   convert -density 200 "$i" "$png" ;
-#   tesseract "$png" "test_$name" ;
-# done
-
-#!/bin/bash
-
-# if ! [ -d output ]; then
-#   mkdir output
-# fi
-
 # For every .txt file within r_string_diff folder
 for filename in *.txt ; do
   # Extract just the name of the file without extention
@@ -27,6 +13,22 @@ for filename in *.txt ; do
   magick $jpg.jpg -dither Riemersma -colors 2 $jpg-d.jpg
   # Do OCR on the created .jpg file and export
   tesseract $jpg-d.jpg "$jpg-converted"
+done
+
+##---------------------------- GARBAGE ----------------------------##
+  # convert -density 200 Hládek2017_Article_LearningStringDistanceWithSmoo.pdf test1.png
+  # for i in *.pdf ; do
+  #   name=${i%.*}
+  #   png=$name.png
+  #   convert -density 200 "$i" "$png" ;
+  #   tesseract "$png" "test_$name" ;
+  # done
+
+  #!/bin/bash
+
+  # if ! [ -d output ]; then
+  #   mkdir output
+  # fi
 
   # echo ${filename%.*}
   # echo $jpg
@@ -34,4 +36,3 @@ for filename in *.txt ; do
   # /Applications/LibreOffice.app/Contents/MacOS/soffice --headless --norestore --writer --convert-to jpg "$filename"
   #/Applications/LibreOffice.app/Contents/MacOS/soffice --convert-to jpg "$filename"
   #tesseract $jpg.jpg "$jpg-converted"
-done
