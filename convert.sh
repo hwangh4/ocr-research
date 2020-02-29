2c86925a8bbd0475bd2d26c9c000f27f4300ddd8
Ott Toomet <otoomet@gmail.com>
2020-02-29 14:54:48 -0800

    convert.sh works on my computer, accepts -i input file

--- convert.sh
+++ convert.sh
@@ -2,23 +2,53 @@
 # OCR Text-image File Conversion Script
 # January 10th, 2020
 
-# For every .txt file within r_string_diff folder
-for filename in *.txt ; do
-  # if splitting a file needed, use code below
-  split $filename file
-done
+if [ $USER == "siim" ]; then
+    SOFFICE=$(which soffice)
+    # note: soffice program must not be running on background!
+    MAGICK=convert
+else
+    SOFFICE=/Applications/LibreOffice.app/Contents/MacOS/soffice
+    MAGICK=magick
+fi
 
-for subfile in file* ; do
+LINES=50  # lines per page
 
+case $1 in
+    -i|--input)
+	FILE="$2"
+	shift # past argument
+	shift # past value
+    ;;
+    -h|--help)
+	echo "usage: $0 -i inputfile"
+	echo "  -l <n> : put n lines per page"
+	exit 0
+	;;
+    -i|--input)
+	LINES="$2"
+	shift # past argument
+	shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+
+basename=${FILE%.*}
+split -d -l 50 $FILE "${basename}-chunk-"
+# use digits to distinguish chunks, put 25 lines per page
+
+for subfile in ${basename}-chunk-* ; do
+    echo $subfile
     # Extract just the name of the file without extention
-    name=$subfile
 
     # Convert .txt to .jpg (ONLY FIRST PAGE WORKS) and export
-    /Applications/LibreOffice.app/Contents/MacOS/soffice --convert-to jpg $name $name
+    $SOFFICE --convert-to jpg $subfile
 
     # # Add dither
-    magick $name.jpg -dither Riemersma -colors 2 $name-dithered.jpg
+    $MAGICK $subfile.jpg -dither Riemersma -colors 2 $subfile-dithered.jpg
     #
     # # Do OCR on the created .jpg file and export
-    tesseract $name-dithered.jpg "$name-converted"
+    tesseract $subfile-dithered.jpg "$subfile-converted"
 done
