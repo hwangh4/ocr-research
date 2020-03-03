b013e6c2ec9c1e291138115a0698c5d5f519d50f
Ott Toomet <otoomet@uw.edu>
2020-03-03 12:18:59 -0800

    converting text to jpg using imagemagick

--- convert.sh
+++ convert.sh
@@ -2,11 +2,13 @@
 # OCR Text-image File Conversion Script
 # January 10th, 2020
 
-if [ $USER == "siim" ]; then
+if [ $USER == "siim" ] || [ $USER == "otoomet" ] ; then
+    echo "Ott"
     SOFFICE=$(which soffice)
     # note: soffice program must not be running on background!
     MAGICK=convert
 else
+    echo "Scarlett"
     SOFFICE=/Applications/LibreOffice.app/Contents/MacOS/soffice
     MAGICK=magick
 fi
@@ -47,6 +49,9 @@ if [ "$basename" == "" ]; then
     echo "please specify the input file"
     exit 1
 fi
+
+echo "splitting $FILE into chunks of $LINE lines"
+
 split -l $LINES $FILE "${basename}-chunk-" || exit 2
 # use digits to distinguish chunks, put 25 lines per page
 
@@ -54,9 +59,15 @@ for subfile in ${basename}-chunk-* ; do
     echo $subfile
     # Extract just the name of the file without extention
 
-    # Convert .txt to .jpg (ONLY FIRST PAGE WORKS) and export
-    $SOFFICE --convert-to jpg $subfile
-
+    ## Convert text to jpg
+    ## Play with font and point size
+    ## Uses "annotate", intended for adding text to images
+    convert -size 3000x2000 xc:white -font "Times" -pointsize 12\
+	    -fill black\
+	    -annotate +15+15 "@$subfile"\
+	    -trim -bordercolor "#FFF" -border 20 +repage\
+	    $subfile.jpg
+    
     # # Add dither
     $MAGICK $subfile.jpg -dither Riemersma -colors 2 $subfile-dithered.jpg
     #
