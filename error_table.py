04ff7f014e8ce2e3fa56983e52c0e69664050ce1
Ott Toomet <otoomet@gmail.com>
2020-04-22 22:09:21 -0700

    error-table: can handle multi-letter swapping

--- error_table.py
+++ error_table.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # Error Table Generation Code Using Python difflib Library
@@ -16,23 +16,46 @@ parser = argparse.ArgumentParser()
 parser.add_argument("directory")
 args = parser.parse_args()
 
+def deleted(diffs, i):
+    """
+    extract a consequtive sequence of deleted characters from diff list at index i
+    """
+    d = list()
+    while  (i < len(diffs)) and (diffs[i][0] == '-'):
+        # note: first test length and thereafter the character!
+        d.append(diffs[i][2])
+        i += 1
+    return d
+
+
+def inserted(diffs, i):
+    """
+    extract a consequtive sequence of inserted characters from diff list at index i
+    """
+    d = list()
+    while  (i < len(diffs)) and (diffs[i][0] == '+'):
+        d.append(diffs[i][2])
+        i += 1
+    return d
+
+
 # Iterate n file names from the given directory
 def iterate_two(iterable, n):
     "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
     return zip(*[iter(iterable)]*n)
 
-# print(sorted(os.listdir(args.directory)))
-
 # Initiate dictionary generation function
 def make_dict(list):
+    """
+    Create the dictionary of character translations
+    inputs:
+    list: list of trigrams from ndiff
+    """
     global table
     global total_count
-    i = -1
-
+    i = 0
     # iterate through char array
-    while i < len(list) - 1:
-        total_count += 1
-        i += 1
+    while i + 1 < len(list):
         indicator = list[i][0]
         ch = list[i][2]
 
@@ -41,46 +64,54 @@ def make_dict(list):
             chdict = table.get(ch, {})
             chdict[ch] = chdict.get(ch, 0) + 1
             table[ch] = chdict
+            i += 1
+            total_count += 1
             continue
 
-        # 2) SUBSTITUTED - if char is not the last character and +/- follows -/+
+        ## 2) SUBSTITUTED - if char is not the last character and +.. is folowed by -.. or the way around
+        ## we make two lists of successive insertions and deletions, and thereafter
+        ## assume that each deletion was replaced by insertion
+        ## The leftover deletions/insertions are assumed to be just that: deletions or insertions
         if i + 1 < len(list):
-
-            # adjacent character and its indicator
-            ch1_ind = list[i + 1][0]
-            ch1 = list[i + 1][2]
-
-            # (this letter was added first)
-            if (indicator == "+" and ch1_ind == "-"):
-                chdict = table.get(ch1, {})
-                chdict[ch] = chdict.get(ch, 0) + 1
-                table[ch1] = chdict
-                i += 1 # increment i and skip next char
-                continue
-
-            # (this letter was deleted first)
-            if (indicator == "-" and ch1_ind == "+"):
-                chdict = table.get(ch, {})
-                chdict[ch1] = chdict.get(ch1, 0) + 1
-                table[ch] = chdict
-                i += 1 # increment i and skip next char
-                continue
-
-        # 3) DELETED - if char is not followed by another indicator and this indicator is "-"
-        if indicator == "-":
-            chdict = table.get(ch, {})
-            chdict["Del"] = chdict.get("Del", 0) + 1
-            table[ch] = chdict
-
-        # 4) INSERTED - if char is not followed by another indicator and this indicator is "+"
-        elif indicator == "+":
-            chdict = table.get("Del", {})
-            chdict[ch] = chdict.get(ch, 0) + 1
-            table["Del"] = chdict
+            ## create two lists--ilist and dlist--for inserted and deleted
+            ## characters
+            if indicator == "-":
+                dlist = deleted(list, i)
+                ilist = inserted(list, i + len(dlist))
+            elif indicator == "+":
+                ilist = inserted(list, i)
+                dlist = deleted(list, i + len(ilist))
+            ## walk over the lists and mark transforms
+            ic = -1
+            for ic in range(min(len(ilist), len(dlist))):
+                cho = dlist[ic]  # original character
+                chc = ilist[ic]  # converted character
+                chdict = table.get(cho, {})
+                chdict[chc] = chdict.get(chc, 0) + 1
+                table[cho] = chdict
+            ## check if any leftover insertions/deletions
+            ## do we have more deletions?
+            ic += 1
+            while ic < len(dlist):
+                cho = dlist[ic]
+                chdict = table.get(cho, {})
+                chdict["Del"] = chdict.get("Del", 0) + 1
+                table[cho] = chdict
+                ic += 1
+            ## or maybe more insertions?
+            while ic < len(ilist):
+                chc = ilist[ic]
+                chdict = table.get("Del", {})
+                chdict[chc] = chdict.get(chc, 0) + 1
+                table["Del"] = chdict
+                ic += 1
+            i += len(dlist) + len(ilist)
+            total_count += len(dlist)
 
         # 5) OTHER - pass any edge cases for now (never found so far)
         else:
             print("-- something weird going on ---", indicator, ch)
+            i += 1
             pass
 
 # Initiate dictionary, read in matching converted and original files from the directory,
@@ -88,7 +119,6 @@ def make_dict(list):
 table = {}
 total_count = 0
 for c, o in iterate_two(sorted(os.listdir(args.directory)), 2):
-
     ## parameterize the file names for command line inputs
     # parser = argparse.ArgumentParser()
     # parser.add_argument("original")
@@ -96,7 +126,6 @@ for c, o in iterate_two(sorted(os.listdir(args.directory)), 2):
     # args = parser.parse_args()
     o = open((args.directory + "/" + o), "r").read()
     c = open((args.directory + "/" + c), "r").read()
-
     # Alter format
     # strip new line characters
     o = [x.strip() for x in o.split("\n")]
@@ -106,9 +135,8 @@ for c, o in iterate_two(sorted(os.listdir(args.directory)), 2):
 
     # Initiate and generate dictionary for each line
     for ol, cl in zip(o,c):
-        n = dl.ndiff(ol, cl)
-        list = [i for i in n]
-        make_dict(list)
+        n = list(dl.ndiff(ol, cl))
+        make_dict(n)
     print("total_count ", total_count)
 
 # Quality check - print dictionary
