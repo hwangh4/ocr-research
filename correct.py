b1599c7a2b3efceb32054340e8b7371830c692bf
Ott S. Toomet <otoomet@tesseract.csde.washington.edu>
2020-05-20 20:42:47 -0700

    parallelism work well, 30-fold the best on tesseract

--- correct.py
+++ correct.py
@@ -111,6 +111,8 @@ def search_dict(token_conv, token_orig):
 ## ------------- MAIN: TEST THE CODE -----------------##
 parser = argparse.ArgumentParser()
 parser.add_argument("table")
+parser.add_argument('--parallel', '-p', type=int, default=2,
+                    help='Level of parallelism')
 # parser.add_argument("textfile")   # uncomment for input command-line arg
 args = parser.parse_args()
 
@@ -120,24 +122,24 @@ table = load_table(error_table_fname)
 corpus = load_corpus()
 
 # -----------SHORT DEBUGGING TEXT--------
-test = """
-ćontragravity lorries were driffing back and forth,
-"""
-# ----------LONG DEBUGGING TEXT----------
 # test = """
-# ćontragravity lorries were driffing back and forth, scattering
-# fertilizer, mainly nitrates from Mimir or Yggarasill. There were stit
-# a good number of animal-drawn plows ahd harrows in use, however.
-#
-# As planots went, Uiler was no bargain, he thought soury.Attimes, he
-# wished he had never followed the lure of rapid promotion and
-# fantastically high pay and left the Federation regulars for the army
-#
-# at the Uiler Company, the hadn't e'd probably be a colonel, zt
-#
-# five thousand sols a year, but maybe it would be better to be a
-# middle-aged colonel cn a decent planet-Odin, with its two moons,
+# ćontragravity lorries were driffing back and forth,
 # """
+# ----------LONG DEBUGGING TEXT----------
+test = """
+ćontragravity lorries were driffing back and forth, scattering
+fertilizer, mainly nitrates from Mimir or Yggarasill. There were stit
+a good number of animal-drawn plows ahd harrows in use, however.
+
+As planots went, Uiler was no bargain, he thought soury.Attimes, he
+wished he had never followed the lure of rapid promotion and
+fantastically high pay and left the Federation regulars for the army
+
+at the Uiler Company, the hadn't e'd probably be a colonel, zt
+
+five thousand sols a year, but maybe it would be better to be a
+middle-aged colonel cn a decent planet-Odin, with its two moons,
+"""
 
 # ----------ACTUAL INPUT----------
 # test = open(args.textfile).read()  # uncomment for input command-line arg
@@ -150,7 +152,7 @@ punc = str.maketrans('', '', string.punctuation)
 tokens = [w.translate(punc) for w in tokens]
 
 # Create multiprocessing pool
-pool_size = 4
+pool_size = args.parallel
 p = Pool(processes = pool_size)
 print(p._processes)
 
